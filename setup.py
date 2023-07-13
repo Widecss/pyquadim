78b85597a42bde20f1e8f59ba39db857b7c1f81a
Widecss <Widecss@users.noreply.github.com>
2023-07-13 12:42:14 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,16 @@ from setuptools_rust import Binding, RustExtension
 setup(
     name="pyquadim",
     version="0.1.0",
+    author="Widecss",
+    author_email="widecss@gmail.com",
+    description="Quadim 到 Python 的简单移植。",
+    readme = "README.md",
+    project_urls={
+        "Documentation": "https://github.com/Widecss/wiki/",
+        "Code": "https://github.com/Widecss/pyquadim/",
+        "Issue Tracker": "https://github.com/Widecss/pyquadim/issues",
+    },
+    platforms=["macOS", "Windows", "X11"],
     rust_extensions=[RustExtension("pyquadim.pyquadim", binding=Binding.PyO3)],
     packages=["pyquadim"],
     # rust extensions are not zip safe, just like C-extensions.
